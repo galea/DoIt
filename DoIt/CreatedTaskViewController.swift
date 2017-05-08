a622ef9cd38abbffb077fa87b4f8b5608cacf511
Bogdan A Galea <bogdan.galea84@gmail.com>
2017-05-08 21:18:43 +0200

    tableview, view controllers and segues

---
+++ DoIt/CreatedTaskViewController.swift
@@ -0,0 +1,38 @@
+//
+//  CreatedTaskViewController.swift
+//  DoIt
+//
+//  Created by Bogdan A Galea on 2017-05-08.
+//  Copyright © 2017 WhiteBelt Apps. All rights reserved.
+//
+
+import UIKit
+
+class CreatedTaskViewController: UIViewController {
+    
+    @IBOutlet weak var taskNameTextField: UITextField!
+    @IBOutlet weak var importantSwitch: UISwitch!
+
+    //connection to get to previcus viewcontroller
+    var previousVC = TasksViewController()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+        
+        //create a task from the textfield
+        let task = Task()
+        task.name = taskNameTextField.text!
+        task.important = importantSwitch.isOn
+        
+        //add new task to array in tableviewController
+        
+        
+    }
+  
+}
---
+++ DoIt/task.swift
@@ -0,0 +1,17 @@
+//
+//  task.swift
+//  DoIt
+//
+//  Created by Bogdan A Galea on 2017-05-08.
+//  Copyright © 2017 WhiteBelt Apps. All rights reserved.
+//
+
+import Foundation
+
+
+class Task {
+    
+    var name = ""
+    var important = false
+    
+}
